257d08aa0a0eafbab4a20fdf7567eb273ff6362c
JLdiesel <1317662314@qq.com>
2021-11-02 14:07:36 +0800

    1

--- 算法TS/动态规划/CoinChange.ts
+++
@@ -1,6 +0,0 @@
-function coin(money: number) {
-  if (money < 1) return Number.MAX_VALUE;
-  if (money === 25 || money == 20 || money === 5 || money === 1) return 1;
-   return Math.min(coin(money-25),coin(money-20),coin(money-5),coin(money-1))+1
-}
-console.log(coin(41));
