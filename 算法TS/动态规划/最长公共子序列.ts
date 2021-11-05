bbbf4754fbed3a39b108c64483c43b6ae37c5d61
JLdiesel <1317662314@qq.com>
2021-11-05 14:54:16 +0800

    最长公共子序列优化1

--- 算法TS/动态规划/最长公共子序列.ts
+++ 算法TS/动态规划/最长公共子序列.ts
@@ -17,12 +17,13 @@ function lcsMain(nums1: number[], i: number, nums2: number[], j: number) {
 function lcs2(nums1: string, nums2: string) {
   if (!nums1 || nums1.length === 0) return 0;
   if (!nums2 || nums2.length === 0) return 0;
-  const dp: number[][] = new Array(nums1.length + 1).fill(
-    Array(nums2.length + 1)
-  );
-  dp[0].fill(0);
+  const dp: number[][] = new Array(nums1.length + 1);
   for (let i = 0; i <= nums1.length; i++) {
+    dp[i] = new Array(nums2.length + 1);
     dp[i][0] = 0;
+  }
+  for (let i = 1; i <= nums2.length; i++) {
+    dp[0][i] = 0;
   }
   for (let i = 1; i <= nums1.length; i++) {
     for (let j = 1; j <= nums2.length; j++) {
@@ -33,8 +34,36 @@ function lcs2(nums1: string, nums2: string) {
       }
     }
   }
+  // console.log(dp);
+
   return dp[nums1.length][nums2.length];
 }
-console.log(lcs2('abcba', 'abcbcba'));
+//时间空间 2*nums2的长度
+function lcs3(nums1: string, nums2: string) {
+  if (!nums1 || nums1.length === 0) return 0;
+  if (!nums2 || nums2.length === 0) return 0;
+  const dp: number[][] = new Array(2);
+  for (let i = 0; i <= 2; i++) {
+    dp[i] = new Array(nums2.length + 1);
+    dp[i][0] = 0;
+  }
+  for (let i = 1; i <= nums2.length; i++) {
+    dp[0][i] = 0;
+  }
+  for (let i = 1; i <= nums1.length; i++) {
+    const row = i & 1;
+    const prevRow = (i - 1) & 1;
+    for (let j = 1; j <= nums2.length; j++) {
+      if (nums1[i - 1] === nums2[j - 1]) {
+        dp[row][j] = dp[prevRow][j - 1] + 1;
+      } else {
+        dp[row][j] = Math.max(dp[prevRow][j], dp[row][j - 1]);
+      }
+    }
+  }
+  return dp[nums1.length & 1][nums2.length];
+}
+
+console.log(lcs3('abcba', 'abcbcba'));
 
 //dp(i,j)是[nums1前i个元素]与[nums2前j个元素]的最长公共子序列长度
