cdddcd0c196f0eb859a17277415b6f31eb30bc43
JLdiesel <1317662314@qq.com>
2021-11-09 11:21:53 +0800

    最长上升子序列二分查找法

--- 算法TS/动态规划/最长上升子序列.ts
+++ 算法TS/动态规划/最长上升子序列.ts
@@ -1,4 +1,4 @@
-const nums = [10, 2, 2, 5, 1, 7, 101, 18];
+const nums = [10, 7, 101, 18];
 function longestUp(nums: number[]) {
   if (!nums || nums.length === 0) return 0;
   const dp: number[] = new Array(nums.length).fill(1);
@@ -14,5 +14,51 @@ function longestUp(nums: number[]) {
 
   return max;
 }
-longestUp(nums);
+//纸牌法
+function longestUp2(nums: number[]) {
+  if (!nums || nums.length === 0) return 0;
+  let len: number = 0;
+  let top: number[] = new Array(nums.length);
+  for (const num of nums) {
+    let j = 0;
+    while (j < len) {
+      if (top[j] >= num) {
+        top[j] = num;
+        break;
+      }
+      j++;
+    }
+    if (j === len) {
+      len++;
+      top[j] = num;
+    }
+  }
+  return len;
+}
+//纸牌法二分查找优化
+function longestUp3(nums: number[]) {
+  if (!nums || nums.length === 0) return 0;
+  let len: number = 0;
+  let top: number[] = new Array(nums.length);
+  for (const num of nums) {
+    let begin = 0;
+    let end = len;
+    while (begin < end) {
+      let mid = (begin + end) >> 1;
+      if (num <= top[mid]) {
+        //右开
+        end = mid;
+      } else {
+        //左闭
+        begin = mid + 1;
+      }
+    }
+    top[begin] = num;
+    if (begin === len) len++;
+  }
+  return len;
+}
+
+console.log(longestUp3(nums));
+
 export {};
--- 算法TS/动态规划/最长公共子串.ts
+++ 算法TS/动态规划/最长公共子串.ts
@@ -9,42 +9,66 @@ function lcs(nums1: string, nums2: string) {
   for (let i = 1; i <= nums2.length; i++) {
     dp[0][i] = 0;
   }
-  let max=0
+  let max = 0;
   for (let i = 1; i <= nums1.length; i++) {
-   for (let j = 1; j <= nums2.length; j++) {
-     if (nums1[i - 1] === nums2[j - 1]) {
-       dp[i][j] = dp[i - 1][j - 1] + 1;
-       max=Math.max(dp[i][j],max)
-     } else {
-       dp[i][j] = 0;
-     }
-   }
+    for (let j = 1; j <= nums2.length; j++) {
+      if (nums1[i - 1] === nums2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+        max = Math.max(dp[i][j], max);
+      } else {
+        dp[i][j] = 0;
+      }
+    }
   }
   console.log(max);
 }
 function lcs2(nums1: string, nums2: string) {
   if (!nums1 || nums1.length === 0) return 0;
   if (!nums2 || nums2.length === 0) return 0;
-  let rowsChars = nums1, colsChars = nums2
+  let rowsChars = nums1,
+    colsChars = nums2;
   if (nums1.length < nums2.length) {
-    colsChars = nums1
-    rowsChars=nums2
+    colsChars = nums1;
+    rowsChars = nums2;
   }
   const dp: number[] = new Array(colsChars.length + 1).fill(0);
-  let max=0
+  let max = 0;
   for (let row = 1; row <= rowsChars.length; row++) {
-    let cur=0
+    let cur = 0;
     for (let col = 1; col <= colsChars.length; col++) {
       let leftTop = cur;
-      cur=dp[col]
-     if (nums1[row - 1] === nums2[col - 1]) {
-       dp[col] =leftTop + 1;
-       max=Math.max(dp[col],max)
-     } else {
-       dp[col] = 0;
-     }
-   }
+      cur = dp[col];
+      if (nums1[row - 1] === nums2[col - 1]) {
+        dp[col] = leftTop + 1;
+        max = Math.max(dp[col], max);
+      } else {
+        dp[col] = 0;
+      }
+    }
   }
- return max
+  return max;
 }
-console.log(lcs2('ADCBA','ABCBA'));
+function lcs3(nums1: string, nums2: string) {
+  if (!nums1 || nums1.length === 0) return 0;
+  if (!nums2 || nums2.length === 0) return 0;
+  let rowsChars = nums1,
+    colsChars = nums2;
+  if (nums1.length < nums2.length) {
+    colsChars = nums1;
+    rowsChars = nums2;
+  }
+  const dp: number[] = new Array(colsChars.length + 1).fill(0);
+  let max = 0;
+  for (let row = 1; row <= rowsChars.length; row++) {
+    for (let col = colsChars.length; col >= 1; col--) {
+      if (nums1[row - 1] === nums2[col - 1]) {
+        dp[col] = dp[col - 1] + 1;
+        max = Math.max(dp[col], max);
+      } else {
+        dp[col] = 0;
+      }
+    }
+  }
+  return max;
+}
+console.log(lcs3('ADCBA', 'ABCBA'));
