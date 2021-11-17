ceb2d72d2ebdc838bc44b27c4759649de357b0e8
JLdiesel <1317662314@qq.com>
2021-11-17 19:03:47 +0800

    59_滑动窗口的最大值

--- leetcode/栈_队列/59_滑动窗口的最大值.ts
+++ leetcode/栈_队列/59_滑动窗口的最大值.ts
@@ -1,5 +1,5 @@
 //双端队列
-function maxSlidingWindow(nums: number[], k: number): number[] {
+function maxSlidingWindowDeque(nums: number[], k: number): number[] {
   if (nums === null || nums.length === 0 || k < 1) return [];
   if (k === 1) return nums;
   const maxes = new Array(nums.length - k + 1);
@@ -21,6 +21,37 @@ function maxSlidingWindow(nums: number[], k: number): number[] {
     }
     //设置窗口的最大值
     maxes[w] = nums[deque[0]];
+  }
+  return maxes;
+}
+function maxSlidingWindow(nums: number[], k: number): number[] {
+  if (nums === null || nums.length === 0 || k < 1) return [];
+  if (k === 1) return nums;
+  const maxes = new Array(nums.length - k + 1);
+  let maxIndex = 0;
+  //先求出前K个元素的最大值索引
+  for (let i = 1; i < k; i++) {
+    if (nums[i] > nums[maxIndex]) {
+      maxIndex = i;
+    }
+  }
+  maxes[0] = nums[maxIndex];
+  //li 是滑动窗口的最左索引，ri是滑动窗口的最右索引
+  for (let li = 1; li < maxes.length; li++) {
+    let ri = li + k - 1;
+    if (maxIndex < li) {
+      //最大值的索引不在滑动窗口的合理范围内
+      maxIndex = li;
+      for (let i = li + 1; i <= ri; i++) {
+        if (nums[i] >= nums[maxIndex]) {
+          maxIndex = i;
+        }
+      }
+    } else if (nums[ri] >= nums[maxIndex]) {
+      //最大值的索引在滑动窗口的合理范围内
+      maxIndex = ri;
+    }
+    maxes[li] = nums[maxIndex];
   }
   return maxes;
 }
