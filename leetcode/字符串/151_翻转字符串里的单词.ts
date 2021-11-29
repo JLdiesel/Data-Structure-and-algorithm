e5b9bfe276c8c4c6d64ece02e49669e55f8ad364
JLdiesel <1317662314@qq.com>
2021-11-29 15:18:48 +0800

    151_翻转字符串里的单词

--- leetcode/字符串/151_翻转字符串里的单词.ts
+++ leetcode/字符串/151_翻转字符串里的单词.ts
@@ -2,24 +2,22 @@ function reverseWords(s: string): string {
   if (!s) return s;
   let len = 0; //字符串最终的有效长度
   let cur = 0; //当前用来存放字符的位置
-  let firstSpace = false; //是否是第一次遇到空格
+  let firstSpace = true; //是否是第一次遇到空格
   const arr = [...s];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== ' ') {
       //非空格字符
       arr[cur++] = arr[i];
-      firstSpace = true;
-    } else if (firstSpace) {
+      firstSpace = false;
+    } else if (!firstSpace) {
       //空格字符 arr[i]是空格字符 arr[i-1]是非空格字符
       arr[cur++] = ' ';
-      firstSpace = false;
+      firstSpace = true;
     }
   }
-  len = firstSpace ? cur : cur - 1;
-  console.log(len);
+  len = firstSpace ? cur - 1 : cur;
 
   if (len <= 0) return '';
-  console.log(arr);
 
   //对整一个有效字符串进行逆序
   reverse(arr, 0, len);
@@ -30,12 +28,13 @@ function reverseWords(s: string): string {
   for (let i = 0; i < len; i++) {
     if (arr[i] === ' ') {
       reverse(arr, prevIndex + 1, i);
-      prevIndex = len;
+      prevIndex = i;
+      console.log(arr, i);
     }
   }
   reverse(arr, prevIndex + 1, len);
   //   const str = arr.toString().replace(/,/g, '');
-  return arr.join('');
+  return arr.slice(0, len).join('');
 }
 //将[li,ri)范围内的字符串进行逆序
 function reverse(arr: string[], li: number, ri: number) {
