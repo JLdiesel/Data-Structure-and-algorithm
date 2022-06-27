b6d2b93f1c41d268ca219b78c7d182495e3798ef
JLdiesel <1317662314@qq.com>
2022-06-27 22:54:03 +0800

    feat: arrayList初始化

--- 数据结构TS/线性表/ArrayList/1_ArrayList.ts
+++ 数据结构TS/线性表/ArrayList/1_ArrayList.ts
@@ -1,8 +1,56 @@
-
-import {ArrayType} from './Interface'
-class ArrayList implements ArrayType{
-  constructor() {
-    
+import { ArrayType } from './Interface';
+class ArrayList<T> implements ArrayType<T> {
+  static DEFAULT_CAPACITY = 10;
+  //元素的长度
+  private _size: number;
+  private elements: T[];
+  constructor(capacity) {
+    if (capacity < ArrayList.DEFAULT_CAPACITY) {
+      this.elements = new Array();
+    } else {
+    }
+  }
+  size(): number {
+    return this._size;
+  }
+  isEmpty(): boolean {
+    return this._size === 0;
+  }
+  contains(element: T): boolean {
+    throw new Error('Method not implemented.');
+  }
+  add(element: T): void;
+  add(index: number, element: T): void;
+  add(index: unknown, element?: T): void {
+    if (element) {
+      console.log(element);
+    } else {
+      this.checkIndex(index as number);
+    }
   }
 
+  get(index: number): T {
+    this.checkIndex(index);
+    return this.elements[index];
+  }
+  set(index: number, element: T): T {
+    this.checkIndex(index);
+    const old = this.elements[index];
+    this.elements[index] = element;
+    return old;
+  }
+  remove(index: number): T {
+    throw new Error('Method not implemented.');
+  }
+  indexOf(element: T): number {
+    throw new Error('Method not implemented.');
+  }
+  clear(): void {
+    throw new Error('Method not implemented.');
+  }
+  checkIndex(index: number) {
+    if (index < 0 || index > this._size) {
+      throw new Error(`index:${index} but size:${this._size}`);
+    }
+  }
 }
