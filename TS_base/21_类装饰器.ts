db5b859c9af924ed79f8789b609f1436adeb39c9
JLdiesel <1317662314@qq.com>
2022-08-03 23:46:23 +0800

    feat: 最小表示法

--- TS_base/21_类装饰器.ts
+++ TS_base/21_类装饰器.ts
@@ -39,6 +39,7 @@ namespace p {
   console.log(Object.getOwnPropertyDescriptors(person));
 }
 namespace q {
+  //如果装饰的是实例属性的话，target是构造函数的原型
   function upperCase(target, key) {
     let oldValue = target[key];
     const getter = () => oldValue;
@@ -52,17 +53,86 @@ namespace q {
       });
     }
   }
+  //如果装饰的是实例属性的话，target是构造函数本身
   function getReal(target, key) {
     console.log(target);
     console.log(key);
+  }
+  function toNumber(target, key, descriptor: PropertyDescriptor) {
+    const oldMethod = descriptor.value;
+    descriptor.value = function (...args) {
+      const newArgs = args.map((item) => parseFloat(item));
+
+      return oldMethod.apply(null, newArgs);
+    };
   }
   class Person {
     @upperCase
     name = 'abc';
     @getReal
     static age = 18;
+    @toNumber
+    sum(...args: any[]) {
+      return args.reduce((prev: number, next: number) => prev + next, 0);
+    }
+  }
+
+  const person = new Person();
+  console.log(person.sum('1', '2', '3'));
+  console.log(parseFloat(['1', '2'] as any));
+
+  console.log(person.name);
+}
+namespace r {
+  //静态成员就是构造函数本身 非静态成员就是构造函数的原型方法名 参数位置
+  function addAge(target, methodName, paramIndex) {
+    console.log(target, methodName, paramIndex);
+  }
+  class Person {
+    age: number;
+    login(userName: string, @addAge password: string) {}
+    login2(@addAge ...arg) {}
+  }
+}
+namespace s {
+  function addAge(target, key, index) {
+    console.log(index);
+  }
+  function staticName(target, key) {
+    console.log(key);
+  }
+  function class1(target) {
+    console.log('class1');
+  }
+  function class2(target) {
+    console.log('class2');
+  }
+  @class1
+  @class2
+  class Person {
+    @staticName
+    age: number;
+    @staticName
+    name: string;
+    @staticName
+    static address: string;
+    @staticName
+    static score: number;
+    @staticName
+    private girl: Person;
+    @staticName
+    private boy: Person;
+    @staticName
+    login(@addAge userName: string, @addAge password: string) {}
+    @staticName
+    login2(
+      @addAge userName: string,
+      @addAge password: string,
+      @addAge age: string
+    ) {}
   }
   const person = new Person();
-  console.log(person.name);
+  // age name girl boy 1 0 address score class2 class1
+  //方法和属性从上到下  参数从后向前 先解析函数参数后解析方法，static滞后 class最后并且从下到上
 }
 export {};
---
+++ 算法TS/最小表示法.ts
@@ -0,0 +1,22 @@
+const s = 'bbacb';
+function getMin(s: string) {
+  let i = 0,
+    j = 1,
+    k = 0,
+    n = s.length;
+  while (i < n && j < n && k < n) {
+    debugger;
+    const a = s[(i + k) % n],
+      b = s[(j + k) % n];
+    if (a == b) k++;
+    else {
+      if (a > b) i += k + 1;
+      else j += k + 1;
+      if (i == j) i++;
+      k = 0;
+    }
+  }
+  i = Math.min(i, j);
+  return s.substring(i) + s.substring(0, i);
+}
+console.log(getMin(s));
