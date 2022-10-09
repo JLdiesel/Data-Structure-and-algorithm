80827a5362817fa20fda06b5db3cc7894b3de3e9
longya <longya@tsign.cn>
2022-10-09 14:09:51 +0800

    feat: 括号的分数

--- leetcode/字符串/856_括号的分数.ts
+++ leetcode/字符串/856_括号的分数.ts
@@ -3,21 +3,42 @@
  * @return {number}
  */
 var scoreOfParentheses = function (s) {
-  if (s.length === 1) return 1
-  let left = 0, prev = 0, score = 0
+  if (s.length === 1) return 1;
+  let left = 0,
+    prev = 0,
+    score = 0;
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === '(') left++
+    if (s[i] === '(') left++;
     else {
-      left--
+      left--;
       if (!left) {
-        if (i - prev === 1) score += 1
+        if (i - prev === 1) score += 1;
         else {
-          score += 2 * scoreOfParentheses(s.slice(prev + 1, i))
+          score += 2 * scoreOfParentheses(s.slice(prev + 1, i));
         }
-        prev = i + 1
+        prev = i + 1;
       }
     }
   }
-  return score
+  return score;
 };
-console.log(scoreOfParentheses("(()(()))"));
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfParentheses = function (s) {
+  let ans = 0,
+    d = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == '(') {
+      ++d;
+    } else {
+      --d;
+      if (s[i - 1] == '(') {
+        ans += 1 << d;
+      }
+    }
+  }
+  return ans;
+};
+console.log(scoreOfParentheses('(()(()))'));
