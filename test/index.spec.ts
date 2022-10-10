e22af60921899aa577592fa85ddba7bf5d858315
longya <longya@tsign.cn>
2022-10-10 10:41:04 +0800

    feat: 801_使序列递增的最小交换次数

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,14 +2,23 @@ describe('ez test', () => {
   beforeEach(() => {
     vi.useFakeTimers();
   });
-
+  function noZero(num) {
+    let count = 0;
+    while (num > 0) {
+      let remainder = num % 10;
+      if (remainder == 0) {
+        return count;
+      }
+      num = (num / 10) | 0;
+      count++;
+    }
+    return true;
+  }
   it('test 1', async () => {
-    const res = await new Promise((res, rej) => {
-      setTimeout(() => {
-        res(123);
-      }, 3000);
-      vi.runAllTimers();
-    });
-    expect(res).toBe(123);
+    expect(noZero(20)).toBe(0);
+    expect(noZero(100)).toBe(0);
+    expect(noZero(1022)).toBe(2);
+    expect(noZero(1002)).toBe(1);
+    expect(noZero(1000)).toBe(0);
   });
 });
